f559e475e3c33f12e30f0c94a0aaf4d6b856318a
Michael Murray <mcmurray0125@gmail.com>
2023-03-26 17:08:03 -0500

    added category filter. updated json data.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,13 @@ import data from "../products.json"
 import ProductCard from './ProductCard'
 
 export default function Home() {
-    const [filter, setFilter] = useState(null)
+    const [priceFilter, setPriceFilter] = useState(null)
+    const [categoryFilter, setCategoryFilter] = useState(null)
     const [showAd, setShowAd] = useState(true);
     const [message, setMessage] = useState('');
-    const minRef= useRef();
-    const maxRef= useRef();
+    const minRef = useRef();
+    const maxRef = useRef();
+    const categoryRef = useRef();
 
     function updateFilter(e) {
         e.preventDefault()
@@ -16,15 +18,23 @@ export default function Home() {
         const max = maxRef.current.value;
 
         if (!min || !max) {
-            setFilter(null);
+            setPriceFilter(null);
         } else if (Number(min) >= Number(max)) {
             setMessage('Min price must be greater than max.');
-            setFilter(null);
+            setPriceFilter(null);
         } else {
             setMessage('');
-            setFilter([min, max]);
+            setPriceFilter([min, max]);
         }
     };
+
+    function updateCategory() {
+        if (categoryRef.current.value === 'all') {
+            setCategoryFilter(null)
+        } else {
+            setCategoryFilter(categoryRef.current.value)
+        }
+    }
       
   return (
     <>
@@ -51,17 +61,36 @@ export default function Home() {
                 </Form.Group>
                 <Button type="submit" onClick={updateFilter}>Update</Button>
             </Form>
+            <Form className='category-filter mt-2'>
+                <Form.Group className='d-flex justify-content-end gap-2 align-items-center'> 
+                    <Form.Label className='m-0' style={{whiteSpace: "nowrap"}}>Category</Form.Label>   
+                    <Form.Select className="quiz-filters" ref={categoryRef} onChange={updateCategory}>
+                        <option value="all">All</option>
+                        <option value="Single Serving">Single Serving</option>
+                        <option value="To Share">To Share</option>
+                    </Form.Select>
+                </Form.Group>
+            </Form>
             {message && <p className='m-0 text-danger w-100 text-end'>{message}</p>}
             <Row className='products-grid py-3'>
                     {(() => {
-                    //Filter Products by Price
+                    //Filter Products by Price & Category
                     const filteredData = data.filter((product) => {
-                        if (!filter) {
+                        if (!priceFilter) {
+                          if (!categoryFilter) {
                             return true;
+                          } else {
+                            return product.category === categoryFilter;
+                          }
+                        } else {
+                          const [min, max] = priceFilter;
+                          if (!categoryFilter) {
+                            return product.price >= min && product.price <= max;
+                          } else {
+                            return product.price >= min && product.price <= max && product.category === categoryFilter;
+                          }
                         }
-                        const [min, max] = filter;
-                        return product.price >= min && product.price <= max;
-                    });
+                      });
                     //No Products Found in Price Filter Range
                     if (filteredData.length === 0) {
                         return (
